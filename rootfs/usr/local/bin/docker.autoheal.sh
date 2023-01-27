df177cd16a48e637d0c670a89406023372734ab9
Bo Wen <bowenwen@users.noreply.github.com>
2023-01-27 19:43:56 +0000

    fix deprecated version of docker compose commands

--- rootfs/usr/local/bin/docker.autoheal.sh
+++ rootfs/usr/local/bin/docker.autoheal.sh
@@ -5,11 +5,11 @@ if [ $(docker ps | grep unhealthy -c) != 0 ] || [ "$FORCE" = "force" ]; then
     if [ -n "$DOCKER_COMPOSE_DIR" ]; then
         echo "Restarting unhealthy vpn app..."
         cd $DOCKER_COMPOSE_DIR
-        docker-compose down
+        docker compose down
         sleep 5
-        docker-compose pull
+        docker compose pull
         sleep 5
-        docker-compose up -d
+        docker compose up -d
     fi
 else
     echo "Looking good, doing nothing..."
